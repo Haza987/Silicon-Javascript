f808a276dcb93b1a48f8885d48d80ada3ac307a6
Harry Holmes <harryholmesse@gmail.com>
2024-10-24 12:03:58 +0200

    Imported styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div class="header-container">
+        
+        <div class="logo">
+          {/* <a id="logo" href="#"><img src="Images/FPImages/Logo.svg" alt="Silicon Logo"></img></a> */}
+          <h3>Silicon</h3>
+        </div>
+  
+        <nav class="navbar">
+          <a class="nav-link" href="#">Features</a>
+        </nav>
+  
+        <div id="darkmode-toggle-switch" class="btn-toggle-switch">
+          <span class="label">Dark mode</span>
+          <label for="darkmode-switch" class="toggle-switch">
+            {/* <input id="darkmode-switch" type="checkbox"> </input> */}
+            <span class="slider round"></span>
+          </label>
+        </div>
+  
+        <a id="auth-signin" href="#" class="btn-primary bgc">
+          <i class="fa-regular fa-user"></i>
+          <span>Sign in / up</span>
+        </a>
+  
+        <button class="btn-mobile">
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      </div>
+    </header>
   )
 }
 
