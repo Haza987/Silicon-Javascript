cc1fc822262e5b1e4b8056639342934c271e0904
Harry Holmes <harryholmesse@gmail.com>
2024-11-07 14:14:11 +0100

    added form validation for contact

--- src/components/contact-components/Consultation.jsx
+++ src/components/contact-components/Consultation.jsx
@@ -1,33 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DropdownMenu from './DropdownMenu'
-import SocialMediaLinks from './SocialMediaLinks'
 
 const Consultation = () => {
+ 
+  const [formData, setFormData] = useState({ fullName: '', email: '', specialist: '', })
+  const [errors, setErrors] = useState({})
+
+  const validateField = (name, value) => {
+    let error = ''
+
+    if (name === 'name' && !/^[A-Öa-ö\s\-]{2,}$/.test(value)) {
+      error = "Must be at least 2 characters long, no numbers."
+    } else if (name === 'email' && !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)) {
+      error = "Enter a valid email address (eg. username@example.com)."
+    } else if (name === 'specialist' && value === "") {
+      error = "Please select a specialist from the drop down menu."
+    }
+
+    setErrors(prevErrors => ({...prevErrors, [name]: error}))
+  }
 
 
+  const validateForm = () => {
+    const newErrors = {};
 
+    if(!/^[A-Öa-ö\s\-]{2,}$/.test(formData.fullName)) {
+      newErrors.fullName = "Must be at least 2 characters long, no numbers."
+    }
+
+
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)) {
+      newErrors.email = "Enter a valid email address (eg. username@example.com).";
+    }
+
+    if (formData.specialist === "") {
+      newErrors.specialist = "Please select a specialist from the drop down menu."
+    }
+
+    setErrors(newErrors)
+    return Object.keys(newErrors).length === 0;
+  }
+
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value})
+
+    validateField(name, value)
+  }
+
+  const handleSubmit = async (e) => {
+    (e).preventDefault();
+
+    if (validateForm()) {
+      console.log('form valid')
+      
+      // const res = await fetch('https://win24-assignment.azurewebsites.net/api/forms/contact', {
+      //   method: 'post',
+      //   headers: {
+      //     'Content-type': 'application/json'
+      //   },
+      //   body: JSON.stringify(formData)
+      // })
+
+      // if (res.ok) {
+      //   const data = await res.json();
+      //   console.log('response:',data)
+      // } else {
+      //   console.log('error code: ' , res.status)
+      //   const errorData = await res.json();
+      //   console.error('Error response:', errorData);
+      // }     
+    } else {
+      console.log('form invalid')
+    }
+    
+  }
+ 
   return (
     <div className="consultation">
       <div className="card">
 
-        <h1>Get Online Consultation</h1>
-
-        <div className="input-group">
-          <label htmlFor="firstName" className="form-label">Full Name</label>
-          <input type="text" id="firstName" className="form-input" placeholder='Name' required/>
+        <div className="header">
+          <h1>Get Online Consultation</h1>
         </div>
 
-        <div className="input-group">
-          <label htmlFor="email" className="form-label">Email Address</label>
-          <input type="email" id="email" className="form-input" placeholder='Email Address' autoComplete='email' required/>
-        </div>
-        
-        <div className="input-group">
-          <label htmlFor="dropdownMenu">Specialist</label>
-          <DropdownMenu/>
-        </div>
+        <form className="consultation-form" onSubmit={handleSubmit} noValidate>
+          <div className="input-group">
+            <label htmlFor="fullName" className="form-label">Full Name</label>
+            <input type="text" name='fullName' className="fullName form-input" placeholder='Full Name' value={formData.fullName} onChange={handleInputChange} required/>
+            {errors.fullName && <span className='validation-error'>{errors.fullName}</span>}
+          </div>
 
-        <a className='appointment bgc'>Make an appointment</a>
+          <div className="input-group">
+            <label htmlFor="email" className="form-label">Email Address</label>
+            <input type="email" name='email' className="email form-input" placeholder='Email Address' autoComplete='email' value={formData.email} onChange={handleInputChange} required/>
+            {errors.email && <span className='validation-error'>{errors.email}</span>}
+          </div>
+          
+          <div className="input-group">
+            <label htmlFor="dropdownMenu">Specialist</label>
+            <DropdownMenu name='specialist' value={formData.specialist} onChange={handleInputChange} required/>
+            {errors.specialist && <span className='validation-error'>{errors.specialist}</span>}
+          </div>
+
+          <button type='submit' className='appointment bgc'>Make an appointment</button>
+        </form>
 
       </div>
     </div>
--- src/components/contact-components/DropdownMenu.jsx
+++ src/components/contact-components/DropdownMenu.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const DropdownMenu = () => {
+const DropdownMenu = ({name, value, onChange}) => {
 
   return (
-    <select className="form-input" id='dropdownMenu'>
-      <option value="placeholder">Select a specialist</option> 
-      <option value="option1">Johnny Cash</option>
-      <option value="option2">50 Cent</option>
-      <option value="option3">Elizabeth Banks</option>
-      <option value="option4">TY Dolla $ign</option>
+    <select className="form-input specialist" id='dropdownMenu' name={name} value={value} onChange={onChange}>
+      <option value="">Select a specialist</option> 
+      <option value="1">Johnny Cash</option>
+      <option value="2">50 Cent</option>
+      <option value="3">Elizabeth Banks</option>
+      <option value="4">TY Dolla $ign</option>
     </select>
     
   );
